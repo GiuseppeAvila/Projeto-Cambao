01cedd529279c0f9fae314a2a17f7525660e8b2b
Gabriela Mitu <gabrielaym@al.insper.edu.br>
2020-05-25 17:50:59 -0300

    complementando layout

---
+++ Projeto2/app/src/main/java/br/edu/insper/al/gabriellaec/projeto2/Info.java
@@ -0,0 +1,14 @@
+package br.edu.insper.al.gabriellaec.projeto2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
