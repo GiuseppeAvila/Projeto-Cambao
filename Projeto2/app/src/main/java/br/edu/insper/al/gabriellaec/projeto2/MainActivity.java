7d8994ce8a92d1a327a7493a243ba4563093a4e1
Gabriella Cukier <you@example.com>
2020-05-31 11:47:37 -0300

    firebase + login

--- Projeto2/app/src/main/java/br/edu/insper/al/gabriellaec/projeto2/MainActivity.java
+++ Projeto2/app/src/main/java/br/edu/insper/al/gabriellaec/projeto2/MainActivity.java
@@ -1,5 +1,6 @@
 package br.edu.insper.al.gabriellaec.projeto2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,10 +8,22 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
+    private EditText emailId, password;
     private Button loginbtn;
     private Button cadastrobtn;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
 
     @Override
@@ -18,30 +31,77 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mFirebaseAuth=FirebaseAuth.getInstance();
+        emailId=findViewById(R.id.usuario);
+        password=findViewById(R.id.senha);
+
         loginbtn = (Button) findViewById(R.id.button3);
         cadastrobtn = (Button) findViewById(R.id.button2);
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                if( mFirebaseUser != null ){
+                    Toast.makeText(MainActivity.this,"Você está logado",Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(MainActivity.this, MainScreen.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(MainActivity.this,"Login",Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openActivityMainScreen();
-            }
-        });
+                    String email = emailId.getText().toString();
+                    String pwd = password.getText().toString();
+                    if(email.isEmpty()){
+                        emailId.setError("Por favor, insira seu email");
+                        emailId.requestFocus();
+                    }
+                    else  if(pwd.isEmpty()){
+                        password.setError("Por favor, insira sua senha");
+                        password.requestFocus();
+                    }
+                    else  if(email.isEmpty() && pwd.isEmpty()){
+                        Toast.makeText(MainActivity.this,"Complete o cadastro",Toast.LENGTH_SHORT).show();
+                    }
+                    else  if(!(email.isEmpty() && pwd.isEmpty())){
+                        mFirebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(!task.isSuccessful()){
+                                    Toast.makeText(MainActivity.this,"Erro no login. Você já possui um cadastro?",Toast.LENGTH_SHORT).show();
+                                }
+                                else{
+                                    Intent intToHome = new Intent(MainActivity.this,MainScreen.class);
+                                    startActivity(intToHome);
+                                }
+                            }
+                        });
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this,"Erro no login",Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }
+            });
 
         cadastrobtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivityCadastro();
-            }
-        });
-    }
+                @Override
+                public void onClick(View v) {
+                    Intent intSignUp = new Intent(MainActivity.this, NovoCadastro.class);
+                    startActivity(intSignUp);
+                }
+            });
+        }
 
-    public void openActivityMainScreen() {
-        Intent intent = new Intent(this, MainScreen.class);
-        startActivity(intent);
+        @Override
+        protected void onStart() {
+            super.onStart();
+            mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+        }
     }
-
-    public void openActivityCadastro() {
-        Intent intent = new Intent(this, NovoCadastro.class);
-        startActivity(intent);
-    }
-}
