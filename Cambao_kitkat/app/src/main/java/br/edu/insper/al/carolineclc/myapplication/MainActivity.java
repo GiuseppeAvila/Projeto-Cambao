a4f6ff2b5bf76a6eefff34be95ea310f1d0c8fa6
carolinechaim <caroline.chaim@gmail.com>
2020-06-01 16:48:46 -0300

    main Activity

--- Cambao_kitkat/app/src/main/java/br/edu/insper/al/carolineclc/myapplication/MainActivity.java
+++ Cambao_kitkat/app/src/main/java/br/edu/insper/al/carolineclc/myapplication/MainActivity.java
@@ -2,13 +2,48 @@ package br.edu.insper.al.carolineclc.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button ButtonLogin;
+    private Button ButtonCadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButtonLogin = (Button) findViewById(R.id.ButtonLogin);
+        ButtonCadastro = (Button) findViewById(R.id.ButtonCadastro);
+
+        ButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityMainScreen();
+            }
+        });
+
+        ButtonCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityCadastro();
+            }
+        });
     }
+
+    public void openActivityMainScreen() {
+        Intent intent = new Intent(this, MainScreen.class);
+        startActivity(intent);
+    }
+
+    public void openActivityCadastro() {
+        Intent intent = new Intent(this, NovoCadastro.class);
+        startActivity(intent);
+    }
+
 }
+
+
