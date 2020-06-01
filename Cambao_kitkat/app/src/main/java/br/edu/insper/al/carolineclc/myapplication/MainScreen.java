3f40cbac6a2ed3f3fcd90150bcbd97a9290be81d
Gabriella Cukier <you@example.com>
2020-06-01 20:34:42 -0300

    funcionando

--- Cambao_kitkat/app/src/main/java/br/edu/insper/al/carolineclc/myapplication/MainScreen.java
+++ Cambao_kitkat/app/src/main/java/br/edu/insper/al/carolineclc/myapplication/MainScreen.java
@@ -131,5 +131,5 @@ public class MainScreen extends AppCompatActivity {
     }
 }
 
-    }
-}
+
+
---
+++ Cambao_kitkat/app/src/main/java/br/edu/insper/al/carolineclc/myapplication/NovoCadastro.java
@@ -0,0 +1,106 @@
+package br.edu.insper.al.carolineclc.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import java.util.Arrays;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+public class NovoCadastro extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    private Button concluir;
+    //FirebaseAuth mFirebaseAuth;
+    private EditText emailId, password, placa, telefone, nome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novo_cadastro);
+
+       // mFirebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.email);
+        password = findViewById(R.id.senha);
+        nome = findViewById(R.id.nome);
+        placa = findViewById(R.id.placa);
+        telefone = findViewById(R.id.telefone);
+        concluir = (Button) findViewById(R.id.button);
+
+
+
+
+        Spinner spinnerCaminhao = (Spinner) findViewById(R.id.spinner);
+        String[] items = new String[] {"Tipo do veículo","Carreta","Carreta LS","Vanderléia","Bitrem","Rodotrem","Truck","Bitruck","VCL","3/4","Toco"};
+        ArrayAdapter<String> adapter =  new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCaminhao.setAdapter(adapter);
+        spinnerCaminhao.setOnItemSelectedListener(this);
+
+
+        Spinner spinnerQuantidade = (Spinner) findViewById(R.id.spinner2);
+        String[] numeros = new String[] {"Quantidade de veículos","1","2","3"};
+        ArrayAdapter<String> adapterNum =  new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, numeros);
+        adapterNum.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerQuantidade.setAdapter(adapterNum);
+        spinnerQuantidade.setOnItemSelectedListener(this);
+
+
+
+        concluir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailId.getText().toString();
+                String pwd = password.getText().toString();
+                String name = emailId.getText().toString();
+                String tel = password.getText().toString();
+                String plate = emailId.getText().toString();
+
+                if (email.isEmpty()) {
+                    emailId.setError("Por favor, informe seu email");
+                    emailId.requestFocus();
+                } else if (pwd.isEmpty()) {
+                    password.setError("Por favor, informe sua senha");
+                    password.requestFocus();
+                } else if (name.isEmpty()) {
+                    password.setError("Por favor, informe seu nome");
+                    password.requestFocus();
+                } else if (tel.isEmpty()) {
+                    password.setError("Por favor, informe seu telefone");
+                    password.requestFocus();
+                } else if (plate.isEmpty()) {
+                    password.setError("Por favor, informe sua placa");
+                    password.requestFocus();
+                } else if (email.isEmpty() && pwd.isEmpty() && name.isEmpty() && tel.isEmpty() && plate.isEmpty()) {
+                    Toast.makeText(NovoCadastro.this, "Crie o cadastro para começar", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });}
+
+
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String text = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        }
+
